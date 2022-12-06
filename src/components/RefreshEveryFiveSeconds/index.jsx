d7432ccc92607f5b13f6473a1e02d6f08235d44e
Sri Hari Anand <srihari@licious.com>
2022-12-06 15:19:01 +0530

    Fixed the refresh count, fixed the grammer

--- src/components/RefreshEveryFiveSeconds/index.jsx
+++ src/components/RefreshEveryFiveSeconds/index.jsx
@@ -8,8 +8,16 @@ const RefreshEveryFiveSecond = props => {
 	return (
 		<>
 			<div className="textContainer refreshEveryFiveSecond">
-				<h3>Refreshes Every Five Second</h3>
-				<div>This refreshed {renderCounter.current} times!</div>
+				<h3>Refreshes every five seconds</h3>
+				<div>
+					{renderCounter.current - 1 <= 0
+						? "This section has not refreshed."
+						: renderCounter.current - 1 === 1
+						? "This section has refreshed once."
+						: `This section has refreshed ${
+								renderCounter.current - 1
+						  } times!`}
+				</div>
 			</div>
 		</>
 	);
